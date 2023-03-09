50af8ca0b9b9c2373563f0fe5ed5e9483067cae5
Jair Gonzales <108778872+jairgonzales3@users.noreply.github.com>
2023-03-09 15:47:12 -0500

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -4,10 +4,8 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `*ESTE ES UN SUB BOT, LA REVENTA ESTA PROHIBIDA, EVITAR SU COMPRA, PARA ADQUIRIR EL BOT HABLAR A LOS SIGUIENTES NUMEROS:*
-Wa.me/+51955095498
-Wa.me/+51933348947
-*Wa.me/+56954390663* ${pesan}`
+let oi = `*LA REVENTA DE ESTE BOT ESTA PROHIBIDA, PARA ADQUIRIRLO COMUNICARSE A :
+Wa.me/+56954390663* ${pesan}`
 let teks = `*RYU BOT LOS INVOCA BOTS*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
 for (let mem of participants) {
 teks += `🐼 @${mem.id.split('@')[0]}\n`}
