2a44cea32bba4022e0e064bd6d764983ab5630cc
Jair Gonzales <108778872+jairgonzales3@users.noreply.github.com>
2023-03-10 00:40:39 -0500

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -6,10 +6,10 @@ throw false
 let pesan = args.join` `
 let oi = `*LA REVENTA DE ESTE BOT ESTA PROHIBIDA, PARA ADQUIRIRLO COMUNICARSE A :
 Wa.me/+56954390663* ${pesan}`
-let teks = `*RYU BOT LOS INVOCA BOTS*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
+let teks = `*ETIQUETA GENERAL DE YAMI BOT*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
 for (let mem of participants) {
 teks += `🐼 @${mem.id.split('@')[0]}\n`}
-teks += `*└*@ffxryu.18\n\n*<3*`
+teks += `*└*@mileeev.e\n\n*<3*`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
