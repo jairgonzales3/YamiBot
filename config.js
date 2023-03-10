c09e46c36cd7ae55650eed830e07a667b785e448
Jair Gonzales <108778872+jairgonzales3@users.noreply.github.com>
2023-03-10 00:39:36 -0500

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 global.owner = [
-  ['51955095498', '@ffxryu.18', true],
+  ['51938775497', 'YamiBot', true],
   ['51955095498', '@ffxryu.18', true],
   ['51955095498', '@ffxryu.18', true],
   ['51955095498', '@ffxryu.18', true],
@@ -25,10 +25,10 @@ global.owner = [
 global.suittag = ['51955095498'] 
 global.prems = ['51955095498'] 
 
-global.packname = '(☞ﾟ∀ﾟ)☞'
-global.author = '@ffxryu.18'
-global.wm = '@ffxryu.18'
-global.igfg = '@ffxryu.18'
+global.packname = 'YamiBot'
+global.author = 'YamiBot'
+global.wm = 'YamiBot'
+global.igfg = 'YamiBot'
 global.wait = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
 
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
@@ -430,9 +430,9 @@ global.mes = d.toLocaleDateString('es', { month: 'long' })
 global.año = d.toLocaleDateString('es', { year: 'numeric' })
 global.tiempo = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
 //*****************************
-global.wm2 = `▸ ${dia} ${fecha}\n▸ @ffxryu.18`
-global.gt = '@ffxryu.18'
-global.mysticbot = '@ffxryu.18'
+global.wm2 = `▸ ${dia} ${fecha}\n▸ YamiBot`
+global.gt = 'YamiBot'
+global.mysticbot = 'YamiBot'
 global.md = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'
 global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'
 global.waitt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
